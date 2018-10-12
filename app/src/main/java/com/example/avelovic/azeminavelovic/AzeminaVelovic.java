bdffa2c0e30211d64e0890d3afa37dc421cce977
Azemina Velovic <avelovic@rogoff.local>
2018-10-11 22:42:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/avelovic/azeminavelovic/AzeminaVelovic.java
@@ -0,0 +1,13 @@
+package com.example.avelovic.azeminavelovic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+ 
+public class AzeminaVelovic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_azemina_velovic);
+    }
+}
